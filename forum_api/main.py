9328db2935d963147c26f315367eaf6eeac29c63
gtmartem <gtmartem@gmail.com>
2019-08-12 13:23:23 +0300

    [init] test case for sections api

--- forum_api/main.py
+++ forum_api/main.py
@@ -4,11 +4,11 @@ from sanic.exceptions import InvalidUsage, ServerError, NotFound
 from forum_api.api.api_versions import api_versions
 from forum_api.errors.handler import error_handler
 
-if __name__ == "__main__":
+app = Sanic()
+app.blueprint(api_versions)
+app.error_handler.add(InvalidUsage, error_handler)
+app.error_handler.add(NotFound, error_handler)
+app.error_handler.add(ServerError, error_handler)
 
-    app = Sanic()
-    app.blueprint(api_versions)
-    app.error_handler.add(InvalidUsage, error_handler)
-    app.error_handler.add(NotFound, error_handler)
-    app.error_handler.add(ServerError, error_handler)
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
